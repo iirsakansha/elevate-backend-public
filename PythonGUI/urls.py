d1e098e8178b3173e068c38ca24d9a5371266ecf
dobariyanirali3006 <85612994+nd18@users.noreply.github.com>
2025-07-01 10:35:02 +0530

    remove admin panel

--- PythonGUI/urls.py
+++ PythonGUI/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('superadmin/', admin.site.urls),
+    # path('superadmin/', admin.site.urls),
     path('', include(('EVTools.urls', 'EVTools'), namespace='EVTools')),
     path('auth/', include('rest_framework.urls')),
     path('admin_panel/', include('admin_panel.urls')),
